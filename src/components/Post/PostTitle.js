31708f332277f6982e78ae766c738ef415dd7e35
Andrew Kachanov <andrew.kachanov@gmail.com>
2017-05-03 03:05:50 +0300

    Обернуть в titleWithLinks()

--- src/components/Post/PostTitle.js
+++ src/components/Post/PostTitle.js
@@ -15,30 +15,34 @@ type Props = {
   isMy?: boolean,
 }
 
-const PostTitle = ({ isTeaser, flow, id, title, isDraft, isMy }: Props) => (
-  <div>
-    <h2>
-      {isTeaser &&
-        <span>
-          <Link className='flow' to={`/flow/${flow.id}/`} onClick={ga('flow', 'feed page', flow.name)}>{flow.name}</Link>
-          <span className='arrow'> → </span>
-          <Link className='link' to={`/post/${id}/`}>{title}</Link>
-        </span>
-        ? null
-        : title
-      }
-      {isDraft && <sup>&nbsp;*&nbsp;черновик</sup>}
-      {isMy && <sup>&nbsp;<Link className='link' to={`/post/edit/${id}/`}>edit</Link></sup>}
-    </h2>
-    <style jsx>{`
-      h2 {
-        font-weight: normal;
-        font-size: 1.5em;
-        line-height: 1.334;
-      }
-    `}</style>
-  </div>
-)
+const PostTitle = ({ isTeaser, flow, id, title, isDraft, isMy }: Props) => {
+  const titleWithLinks = () => (
+    <span>
+      <Link className='flow' to={`/flow/${flow.id}/`} onClick={ga('flow', 'feed page', flow.name)}>{flow.name}</Link>
+      <span className='arrow'> → </span>
+      <Link className='link' to={`/post/${id}/`}>{title}</Link>
+    </span>
+  )
+  return (
+    <div>
+      <h2>
+        {isTeaser
+          ? titleWithLinks()
+          : title
+        }
+        {isDraft && <sup>&nbsp;*&nbsp;черновик</sup>}
+        {isMy && <sup>&nbsp;<Link className='link' to={`/post/edit/${id}/`}>edit</Link></sup>}
+      </h2>
+      <style jsx>{`
+        h2 {
+          font-weight: normal;
+          font-size: 1.5em;
+          line-height: 1.334;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 // PostTitle.propTypes = {
 //   isTeaser: PropTypes.bool,
