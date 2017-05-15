1d3f7f53836b60edbcd71706da1c1a82657aca04
Andrew Kachanov <andrew.kachanov@gmail.com>
2017-05-15 11:43:55 +0300

    Анимировать сердечко

--- src/components/Page/PageFooter.js
+++ src/components/Page/PageFooter.js
@@ -5,7 +5,12 @@ const Footer = () => (
     <div className='copyright'>
       <i className='fa fa-code' aria-hidden='true' />
       &nbsp;with&nbsp;
-      <i className='fa fa-heart' aria-hidden='true' />
+      <svg className='icon-love' width='21' height='17' viewBox='0 0 21 17' xmlns='http://www.w3.org/2000/svg'>
+        <title>love</title>
+        <path d='M14.725.032a5.31 5.31 0 0 0-4.687 2.814 5.312 5.312 0 0 0-10 2.498c0 4.763 5.834 7.397 10 11.564 4.306-4.306 10-6.76 10-11.563A5.312 5.312 0 0 0 14.725.032z'
+          fill='#E82F2F'
+          fillRule='evenodd' />
+      </svg>
       &nbsp;by&nbsp;<a href='//github.com/comerc/' className='developer'>comerc</a>&nbsp;
       <i className='fa fa-copyright' aria-hidden='true' />
       &nbsp;{(new Date()).getFullYear()}&nbsp;
@@ -47,6 +52,26 @@ const Footer = () => (
         display: inline-block;
         white-space: nowrap;
       }
+      .icon-love {
+        animation: a 1.2s ease-in-out infinite;
+      }
+      @keyframes a {
+        0% {
+          transform: scale(1);
+        }
+        14% {
+          transform: scale(1.3);
+        }
+        28% {
+          transform: scale(1);
+        }
+        42% {
+          transform: scale(1.3);
+        }
+        70% {
+          transform: scale(1);
+        }
+      }  
     `}</style>
   </div>
 )
