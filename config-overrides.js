6a891fb23f0c117517fe819cade1d93d4a80c6ed
Andrew Kachanov <andrew.kachanov@gmail.com>
2017-06-05 21:02:53 +0300

    Обновить пакеты и откатить react-scripts

--- config-overrides.js
+++ config-overrides.js
@@ -3,10 +3,12 @@ const babelLoader = function(conf) {
 }
 
 function rewire(config, env) {
+  console.log(config)
   const babelrc = config.module.loaders.find(babelLoader).query
   babelrc.plugins = [
     // не работает jest, заменил на NODE_PATH=src/ в .env
     // ['module-resolver', { 'root': ['src'] }],
+    'lodash',
     'babel-plugin-idx',
     'styled-jsx-postcss/babel',
     // 'flow-react-proptypes',
