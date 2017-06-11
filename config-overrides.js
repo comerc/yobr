ef4edaba6facb4462a1ad026541ba99e7c97fa4a
comerc <comerc@mail.ru>
2017-06-12 00:22:45 +0300

    Исправить подключение babel-плагинов

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,19 @@
 function rewire(config, env) {
-  const babelOptions = config.module.rules.find(
-    conf => conf.loader && conf.loader.includes('babel-loader'),
-  ).options
-  const path = require('path')
-  babelOptions.presets = babelOptions.presets.concat(path.resolve('babel-preset.js'))
+  const babelOptions = config.module.rules.find(conf => {
+    return conf.loader && conf.loader.includes('babel-loader')
+  }).options
+  const babelrc = require(babelOptions.presets[0])
+  babelrc.plugins = [
+    // не работает jest, заменил на NODE_PATH=src/ в .env
+    ['module-resolver', { root: ['src'] }],
+    'lodash',
+    'babel-plugin-idx',
+    'styled-jsx-postcss/babel',
+    // 'flow-react-proptypes',
+    'tcomb',
+  ].concat(babelrc.plugins || [])
+  console.log(babelrc.plugins)
+  babelOptions.presets = babelrc
   return config
 }
 
